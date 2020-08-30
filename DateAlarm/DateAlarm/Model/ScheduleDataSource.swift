f6b8e50d9430b543b33f77471fc890082045578f
ouibene <ouibene@gmail.com>
2020-08-30 21:26:25 +0900

    Add buttons and TableView

---
+++ DateAlarm/DateAlarm/Model/ScheduleDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  ScheduleDataSource.swift
+//  DateAlarm
+//
+//  Created by ye on 2020/08/30.
+//  Copyright © 2020 oui. All rights reserved.
+//
+
+import Foundation
+
+class ScheduleDataSource {
+    
+}
--- DateAlarm/DateAlarm/ViewController.swift
+++ DateAlarm/DateAlarm/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var optionButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController:  UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            /* FIXME! number of rows */
+            //ScheduleDataSource
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
